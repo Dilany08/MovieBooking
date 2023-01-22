2f8ff932919228dccd7f06c4d3cc3fbe2e81b6e9
Dilany08 <dannelramos0802@gmail.com>
2023-01-22 18:47:26 +0800

    action movie done

---
+++ app/src/main/java/com/example/moviebooking/ActionMovie3.java
@@ -0,0 +1,14 @@
+package com.example.moviebooking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActionMovie3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_action_movie3);
+    }
+}
