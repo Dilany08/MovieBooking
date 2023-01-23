d167d46199beabb3753a284ed7c6c2eb3813f245
edusuuu <edzeljohnangeles@gmail.com>
2023-01-23 15:35:14 +0800

    fully functional action movie 1

--- app/src/main/java/com/example/moviebooking/ActionMovie1.java
+++ app/src/main/java/com/example/moviebooking/ActionMovie1.java
@@ -17,12 +17,16 @@ public class ActionMovie1 extends AppCompatActivity {
         setContentView(R.layout.activity_action_movie1);
         TextView title = findViewById(R.id.title);
         TextView time1 = findViewById(R.id.time1);
+        TextView time2 = findViewById(R.id.time2);
+        TextView time3 = findViewById(R.id.time3);
         ImageView back = findViewById(R.id.btnBack);
         ImageView timebtn1 = findViewById(R.id.timeBtn1);
         ImageView timebtn2 = findViewById(R.id.timeBtn2);
         ImageView timebtn3 = findViewById(R.id.timeBtn3);
 
         timebtn1.setClickable(true);
+        timebtn2.setClickable(true);
+        timebtn3.setClickable(true);
         back.setClickable(true);
         timebtn1.bringToFront();
 
@@ -43,6 +47,30 @@ public class ActionMovie1 extends AppCompatActivity {
                 Intent i = new Intent(ActionMovie1.this,TransactionPage.class);
                 i.putExtra("Title", "Title:" + title.getText().toString() );
                 i.putExtra("Time", time1.getText().toString());
+                i.putExtra("Amount", "0.00");
+                startActivity(i);
+            }
+        });
+
+        timebtn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(ActionMovie1.this,TransactionPage.class);
+                i.putExtra("Title", "Title:" + title.getText().toString() );
+                i.putExtra("Time", time2.getText().toString());
+                i.putExtra("Amount", "0.00");
+                startActivity(i);
+
+
+            }
+        });
+
+        timebtn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(ActionMovie1.this,TransactionPage.class);
+                i.putExtra("Title", "Title:" + title.getText().toString() );
+                i.putExtra("Time", time3.getText().toString());
                 i.putExtra("Amount", "0.00");
                 startActivity(i);
             }
--- app/src/main/java/com/example/moviebooking/ActionMovie2.java
+++ app/src/main/java/com/example/moviebooking/ActionMovie2.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class ActionMovie2 extends AppCompatActivity {
@@ -14,6 +15,19 @@ public class ActionMovie2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_action_movie2);
+        TextView title,time1,time2,time3;
+        ImageView timebtn1,timebtn2,timebtn3;
+        title = findViewById(R.id.title);
+        time1 = findViewById(R.id.time1);
+        time2 = findViewById(R.id.time2);
+        time3 = findViewById(R.id.time3);
+        timebtn1 = findViewById(R.id.timeBtn1);
+        timebtn2 = findViewById(R.id.timeBtn2);
+        timebtn3 = findViewById(R.id.timeBtn3);
+
+        timebtn1.setClickable(true);
+        timebtn2.setClickable(true);
+        timebtn3.setClickable(true);
 
         ImageView back = findViewById(R.id.btnBack);
 
