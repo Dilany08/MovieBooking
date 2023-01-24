95fe424613cb3326008e1c4c2eb8404ca478be40
Dilany08 <dannelramos0802@gmail.com>
2023-01-24 14:34:34 +0800

    tapot na kalahati

--- app/src/main/java/com/example/moviebooking/Appdata.java
+++ app/src/main/java/com/example/moviebooking/Appdata.java
@@ -10,25 +10,86 @@ public class Appdata {
     public String amount;
     public String time;
 
-public Appdata() {
-    this.id = "";
-    this.data = "";
-    this.name = "";
-    this.phone = "";
-    this.email = "";
-    this.title = "";
-    this.amount = "";
-    this.time = "";
-}
+    public Appdata() {
+        this.id = "";
+        this.data = "";
+        this.name = "";
+        this.phone = "";
+        this.email = "";
+        this.title = "";
+        this.amount = "";
+        this.time = "";
+    }
 
-public String getData() {
-    return data;
-}
-public void setData(String id,String name, String phone, String email, String title, String time, String amount){
+    public String getData() {
+        return data;
+    }
+    public void setData(String id,String name, String phone, String email, String title, String time, String amount){
         StringBuffer i = new StringBuffer(this.data);
-    i.append("Name: " + name  + "\nPhone Number:  " + phone + "\nE-mail: " + email + "\nTitle:" + title + "     Time: " + time + "\n\n Amount: " + amount + "\n\nTicket Number :" + id + "-769" + id +"15672" + id + "10"  );
-this.data = i.toString();
-}
+        i.append("Name: " + name  + "\nPhone Number:  " + phone + "\nE-mail: " + email + "\nTitle:" + title + "     Time: " + time + "\n\n Amount: " + amount + "\n\nTicket Number :" + id + "-769" + id +"15672" + id + "10"  );
+        this.data = i.toString();
+    }
+
+
+
+
+ public void setName(String name)
+ {
+     this.name = name;
+ }
+
+ public void setPhone(String phone)
+ {
+     this.phone = phone;
+ }
+ public void setEmail(String email)
+ {
+     this.email = email;
+ }
+ public void setTitle(String title)
+ {
+     this.title = title;
+ }
+ public void setTime(String time)
+ {
+     this.time = time;
+ }
+ public void setAmount(String amount)
+ {
+     this.amount = amount;
+ }
+ public void setId(String id)
+ {
+     this.id = id;
+ }
+ public String getName()
+ {
+    return name;
+ }
+ public String getPhone()
+ {
+     return phone;
+ }
+ public String getEmail()
+ {
+     return email;
+ }
+ public String getTitle()
+ {
+     return title;
+ }
+ public String getTime()
+ {
+     return time;
+ }
+ public String getAmount()
+ {
+     return amount;
+ }
+ public String getId()
+ {
+     return id;
+ }
 
 
 }
--- app/src/main/java/com/example/moviebooking/BillingPage.java
+++ app/src/main/java/com/example/moviebooking/BillingPage.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class BillingPage extends AppCompatActivity {
 
@@ -12,7 +15,7 @@ public class BillingPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_billing_page);
-        TextView title1,title2,title3,title4,title5,time1,time2,time3,time4,time5,tvName,tvNumber,tvEmail,price;
+        TextView title1, title2, title3, title4, title5, time1, time2, time3, time4, time5, tvName, tvNumber, tvEmail, price;
         TextView id = findViewById(R.id.id);
 
         tvName = findViewById(R.id.tvName);
